80e55ac2d824939f5e7670645926c688d6faf2c8
gabriely <gabrielysimette@alunos.utfpr.edu.br>
2025-10-31 20:46:46 -0400

    Add script to update vector database

---
+++ update_vector_database.py
@@ -0,0 +1,11 @@
+!pip install databricks-vectorsearch
+dbutils.library.restartPython()
+
+from databricks.vector_search.client import VectorSearchClient
+vector_index_name = "tcc_workspace_catalog.inss_crawled_data.direitos_aposentadoria_index"
+vsc = VectorSearchClient()
+
+index = vsc.get_index(
+    index_name=vector_index_name
+)
+index.sync()
