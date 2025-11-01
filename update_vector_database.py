c0a0e67abf863c176d9e14e105401c18e7bc4668
gabriely <gabrielysimette@alunos.utfpr.edu.br>
2025-10-31 21:15:50 -0400

    Fix notebook notation

--- update_vector_database.py
+++ update_vector_database.py
@@ -2,6 +2,8 @@
 # MAGIC %pip install databricks-vectorsearch
 # MAGIC dbutils.library.restartPython()
 
+# COMMAND ----------
+
 from databricks.vector_search.client import VectorSearchClient
 vector_index_name = "tcc_workspace_catalog.inss_crawled_data.direitos_aposentadoria_index"
 vsc = VectorSearchClient()
